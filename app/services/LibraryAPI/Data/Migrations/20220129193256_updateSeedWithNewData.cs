4a161051f7f22c6591e2422988d91a67e5c38dff
lovrovi <lovrovidovic6@gmail.com>
2022-01-29 20:36:25 +0100

    seed

---
+++ app/services/LibraryAPI/Data/Migrations/20220129193256_updateSeedWithNewData.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryAPI.Data.Migrations
+{
+    public partial class updateSeedWithNewData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Reservations",
+                newName: "TimeStamp");
+
+            migrationBuilder.InsertData(
+                table: "Comments",
+                columns: new[] { "Id", "BookId", "Text", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 1, "first comment", 1 },
+                    { 2, 2, "first comment", 1 },
+                    { 3, 2, "first comment", 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Loans",
+                columns: new[] { "Id", "BookId", "IsReturned", "LoanDate", "ReturnDate", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 1, true, new DateTime(2022, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1 },
+                    { 2, 2, false, new DateTime(2022, 1, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 2, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 3 },
+                    { 3, 3, true, new DateTime(2022, 1, 19, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 2, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), 3 },
+                    { 4, 2, true, new DateTime(2022, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Loans",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Loans",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Loans",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Loans",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.RenameColumn(
+                name: "TimeStamp",
+                table: "Reservations",
+                newName: "Date");
+        }
+    }
+}
